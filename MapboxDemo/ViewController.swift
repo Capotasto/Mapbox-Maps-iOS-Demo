8a287bab206375e5099f80097ac42c7894615d6b
Capotasto <rocker519804@gmail.com>
2022-12-04 12:50:25 +0900

    first commit

--- MapboxDemo/ViewController.swift
+++ MapboxDemo/ViewController.swift
@@ -1,19 +1,22 @@
-//
-//  ViewController.swift
-//  MapboxDemo
-//
-//  Created by Norio Egi on 2022/12/03.
-//
-
 import UIKit
+import MapboxMaps
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    internal var mapView: MapView!
+    private let tokyoStation = CLLocationCoordinate2D(latitude: 35.6812, longitude: 139.7671)
+
+    override public func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let token = Bundle.main.object(forInfoDictionaryKey: "MBXAccessToken") as? String ?? ""
+
+        let option = MapInitOptions(
+            resourceOptions: ResourceOptions(accessToken: token),
+            cameraOptions: CameraOptions(center: tokyoStation, zoom: 14.5)
+        )
+        mapView = MapView(frame: view.bounds, mapInitOptions: option)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        self.view.addSubview(mapView)
     }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '13.0'
+
+target 'MapboxDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MapboxDemo
+  pod 'MapboxMaps', '10.9.1'
+
+end
